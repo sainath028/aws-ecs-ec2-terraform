475538c7a638f61df9089175362b89f19786dd95
sainath028 <sainath028@gmail.com>
2023-03-31 20:27:47 +0530

    Delete deploy directory

--- deploy/local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  instance_ami = data.aws_ami.ecs_ami.id
-  tags = {
-    Environment = var.environment
-    Application = var.app_name
-  }
-  vpc_cidr = "10.0.0.0/16"
-}
--- deploy/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_lb_dns_name" {
-  value = aws_lb.default.dns_name
-}
